a9c2637698dc908e9d501aeb28fd08b7031256c3
rhurtado <r00td3fault@gmail.com>
2025-08-22 15:27:42 -0500

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { OrdersModule } from './orders/orders.module';
 import { CommonModule } from './common/common.module';
+import { EnvValidationSchema } from './config/env-validation.schema';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
-  imports: [OrdersModule, CommonModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: EnvValidationSchema,
+    }),
+    OrdersModule,
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
