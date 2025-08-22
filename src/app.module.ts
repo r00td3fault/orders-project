cf08fb02d17b9bdaf664c2f76bd9661acd19b436
rhurtado <r00td3fault@gmail.com>
2025-08-22 15:21:04 -0500

    inital scafolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [OrdersModule, CommonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
