d5ca02ebdad34fd5a3ffd9563280d5b57fbafe06
rhurtado <r00td3fault@gmail.com>
2025-08-22 16:36:08 -0500

    solved class validation configuration and enhance README

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/interceptors/loggin.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Orders project')
+    .setDescription('Orders API description')
+    .setVersion('1.0')
+    .addTag('Orders')
+    .addGlobalResponse({
+      status: 500,
+      description: 'Internal server error',
+    })
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    jsonDocumentUrl: 'api/swagger/json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
